0062fb9d9a56ece1573921149e03fa62a912a8b6
Jackie Chheng <jackiechheng@Jackies-MacBook-Pro.local>
2023-10-06 23:55:06 -0600

    added localstorage for users, and added divs from 9am-5pm

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,4 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+
 $(function () {
  $(".saveBtn").on("click", function () {
   var timeBlockId = $(this).closest(".time-block").attr("id"); // Finds closest ancestor with timeblock and gets ID
@@ -8,19 +6,33 @@ $(function () {
   localStorage.setItem(timeBlockId, userInput); // Saves user input in local storage
  });
 
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
+  var currentHour = dayjs().format("H"); // Get current hour in 24-hour format
+
+  $(".time-block").each(function () {
+  var blockHour = parseInt($(this).attr("id").split("-")[1]); // Extract hour from the ID
+  if (blockHour < currentHour) {
+    $(this).addClass("past");
+  } else if (blockHour == currentHour) {
+    $(this).addClass("present");
+  } else {
+    $(this).addClass("future");
+  }
+
+
+  // Retrieve and populate saved user data
+  var timeBlockId = $(this).attr("id");
+  var savedUserInput = localStorage.getItem(timeBlockId);
+
+  if (savedUserInput !== null) {
+    $(this).find("textarea").val(savedUserInput);
+  }
+});
+
+
+
+
   var currentDay =dayjs().format("dddd, MMMM D HH:mm") // Displays current time in order from [Day Month, Weekday Time(Military)]
   $("#currentDay").text(currentDay); 
-})
+});
 
 
